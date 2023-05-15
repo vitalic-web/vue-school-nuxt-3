608a937f743a45b318cf729edb209f723f5f2fed
Vitaly <vitalic.web@gmail.com>
2023-05-15 19:39:53 +0300

    Add movies id and teams id pages

---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+const message = computed(()=> `I'm on team ${route.params.teamSlug} and my user id is ${route.params.userId}`)
+</script>
+<template>
+    <p>
+        {{ message }}
+    </p>
+</template>
