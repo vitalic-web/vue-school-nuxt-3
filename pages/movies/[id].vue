4eec3e0d4bb570ac66f1adba45f14e8889207b77
Vitaly <vitalic.web@gmail.com>
2023-05-16 19:41:06 +0300

    Add page for error movie id

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,9 +1,20 @@
 <script setup>
 const route = useRoute();
-const { data } = useFetch(`http://www.omdbapi.com/?apikey=aee0adc2&i=${route.params.id}`, {
-  pick: ['Plot', 'Title'],
+const { data, error } = await useFetch(`http://www.omdbapi.com/?apikey=aee0adc2&i=${route.params.id}`, {
+  pick: ['Plot', 'Title', 'Error'],
   key: `/movies/${route.params.id}`,
 });
+
+if (error.value) {
+  // handle accordingly
+}
+
+if (data.value.Error === 'Incorrect IMDb ID.') {
+  showError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+  })
+}
 </script>
 
 <template>
